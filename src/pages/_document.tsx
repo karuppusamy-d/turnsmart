20095d4accf1d0875cdc21af13eba8e1497184f7
Karuppusamy <d.karuppusamy@outlook.com>
2021-10-15 21:51:28 +0530

    Move favicons from static to public dir

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,33 +8,33 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/favicons/apple-touch-icon.png"
+            href="/favicons/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
+            href="/favicons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
+            href="/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
           <link
             rel="mask-icon"
-            href="/static/favicons/safari-pinned-tab.svg"
+            href="/favicons/safari-pinned-tab.svg"
             color="#2d89ef"
           />
-          <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
           <meta name="apple-mobile-web-app-title" content="Project.io" />
           <meta name="application-name" content="Project.io" />
           <meta name="msapplication-TileColor" content="#2d89ef" />
           <meta
             name="msapplication-config"
-            content="/static/favicons/browserconfig.xml"
+            content="/favicons/browserconfig.xml"
           />
           <meta name="theme-color" content="#ffffff" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
